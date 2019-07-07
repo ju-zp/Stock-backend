e7459b022a839bab862fdfbcb9ed532a1e3c4eea
ju-zp <jhuntington@tutanota.com>
2019-07-07 13:56:03 +0100

    added full crud to product controller and permitted params

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,2 +1,46 @@
 class ProductController < ApplicationController
+
+    before_action :get_product, only: [:show, :update, :destroy]
+
+    def index
+        @products = Product.all
+        render json: @products
+    end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            render json: {status: 200}
+        else
+            render json: {status: 400, message: "Unable to save"}
+        end
+    end
+
+    def show
+        render json: @product
+    end
+
+    def update
+        @product.update(product_params)
+        if @product.save
+            render json: {status: 200}
+        else
+            render json: {status: 400, message: "Unable to update"}
+        end
+    end
+
+    def destroy
+        @product.destroy
+        render json: {status: 200}
+    end
+
+    private 
+
+    def product_params
+        params.require(:product).permit(:name, :description)
+    end
+
+    def get_product
+        @product = Product.find(params[:id])
+    end
 end
