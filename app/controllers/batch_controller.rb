7258db3383cf40b53e1b03d60ff8dcac629b9e6a
ju-zp <jhuntington@tutanota.com>
2019-07-19 15:25:57 +0100

    updated endpoint to return data in correct format

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -2,7 +2,16 @@ class BatchController < ApplicationController
 
   def index
     @batches = Batch.all
-    render json: @batches
+    newArray = []
+    @batches.map{|b|newArray.push(transformBatchObj(b))}
+    render json: newArray
+  end
+
+  private 
+
+  def transformBatchObj(batch)
+    @product = batch.product
+    return {code: batch[:code], quantity: batch[:quantity], best_before: batch[:best_before], sold: batch[:sold], product: @product}
   end
 
 end
