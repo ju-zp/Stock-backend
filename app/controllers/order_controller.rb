28815d13f6744fe6a48ec250a7e92c69d54fcce9
ju-zp <jhuntington@tutanota.com>
2019-07-22 14:42:18 +0100

    added function for formatting order obj into usable data

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -5,4 +5,12 @@ class OrderController < ApplicationController
     render json: @orders
   end
 
+  private
+
+  def formatOrderInfo(order)
+    @batch_orders = order.batch_orders
+    @total = @batch_orders.sum{|b| b.quantity}
+    return {order_ref: order[:order_ref], item_count: @batch_orders.size, quantity: @total}
+  end
+
 end
