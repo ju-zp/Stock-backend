4c6934c4e218068672b80448923572dd0617728e
ju-zp <jhuntington@tutanota.com>
2019-07-24 11:55:30 +0100

    added function for saving batch to database and returning a response

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -7,11 +7,31 @@ class BatchController < ApplicationController
     render json: newArray
   end
 
-  private 
+  def create
+    @batch = Batch.new(batch_params)
+    @product = getProduct(params[:barSlug])
+    @batch.sold = 0
+    @batch.product = @product
+    @batch.best_before = Date.parse(params[:bestBefore])
+    if(@batch.save)
+      render json: {}
+    else
+      render json: {message: 'Unable to save'}, status: 400
+    end
+  end
+
+  private
+
+  def batch_params
+    params.require(:batch).permit(:code, :quantity)
+  end
+
+  def getProduct(slug)
+    Product.find_by({slug: slug})
+  end
 
   def transformBatchObj(batch)
     @product = batch.product
-    # return {:code, :quantity, :best_before, :sold, product: @product.name}
     return {code: batch[:code], quantity: batch[:quantity], best_before: batch[:best_before], sold: batch[:sold], product: @product.name}
   end
 
