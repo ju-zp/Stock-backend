be4a938a63d3fb1cc26367fdb86e2725d2d7779b
ju-zp <jhuntington@tutanota.com>
2019-07-26 12:35:23 +0100

    added functionality to save an order

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -7,7 +7,31 @@ class OrderController < ApplicationController
     render json: newArray
   end
 
+  def create
+    @order = Order.new(order_params)
+    puts @order
+    params[:batches].each do |b|
+      @batch = Batch.find(b[:batch_id])
+      @batch.sold = b[:quantity] + @batch.sold
+      @batch.save
+      @batch_order = BatchOrder.new
+      @batch_order.batch_id = b[:batch_id]
+      @batch_order.quantity = b[:quantity]
+      @batch_order.order = @order
+      @batch_order.save
+    end
+    if @order.save
+      render json: {status: 200}
+    else
+      render json: {status: 400, message: "Unable to save"}
+    end
+  end
+
   private
+
+  def order_params
+    params.require(:order).permit(:order_ref)
+  end
 
   def formatOrderInfo(order)
     @batch_orders = order.batch_orders
