94a46e6a914e721667f986ce560d0cea8d77a857
ju-zp <jhuntington@tutanota.com>
2019-08-13 16:49:04 +0100

    added capability to delete a batch

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -1,15 +1,16 @@
 class BatchController < ApplicationController 
+  before_action :get_batch, only: [:destroy]
 
   def index
     @batches = Batch.all
     newArray = []
-    @batches.map{|b|newArray.push(transformBatchObj(b))}
+    @batches.map{|b|newArray.push(transform_batch_obj(b))}
     render json: newArray
   end
 
   def create
     @batch = Batch.new(batch_params)
-    @product = getProduct(params[:barSlug])
+    @product = get_product(params[:barSlug])
     @batch.sold = 0
     @batch.product = @product
     @batch.best_before = Date.parse(params[:bestBefore])
@@ -20,17 +21,29 @@ class BatchController < ApplicationController
     end
   end
 
+  def destroy
+    if @batch.destroy
+      render json: {}
+    else
+      render json: {message: 'Unable to delete resource'}
+    end
+  end
+
   private
 
   def batch_params
     params.require(:batch).permit(:code, :quantity)
   end
 
-  def getProduct(slug)
+  def get_batch
+    @batch = Batch.find(params[:id])
+  end
+
+  def get_product(slug)
     Product.find_by({slug: slug})
   end
 
-  def transformBatchObj(batch)
+  def transform_batch_obj(batch)
     @product = batch.product
     return {code: batch[:code], quantity: batch[:quantity], best_before: batch[:best_before], sold: batch[:sold], product: @product.name}
   end
--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,5 +1,18 @@
 class Batch < ApplicationRecord
-  belongs_to :product, dependent: :destroy
+  belongs_to :product
   has_many :batch_orders
   has_many :orders, through: :batch_orders
+
+  before_destroy :destroy_batch_orders, :destroy_orders
+
+  private 
+
+  def destroy_batch_orders
+    self.batch_orders.destroy_all
+  end
+
+  def destroy_orders
+    self.orders.destroy_all
+  end
+
 end
