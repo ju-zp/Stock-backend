475ee35c06364e36d50845453e065bbe7f658829
ju-zp <jhuntington@tutanota.com>
2019-09-02 12:22:58 +0100

    updates

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Stock
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://serene-lalande-0d830a.netlify.com'
+        origins ENV['SITE_ORIGIN'] 
         resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
       end
     end
