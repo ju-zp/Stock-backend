b10d9762b040ad623002c99e164c0c9df50367cc
ju-zp <jhuntington@tutanota.com>
2019-09-02 13:08:00 +0100

    updates

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Stock
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'https://amazing-ride-749654.netlify.com/'
-        resource 'https://amazing-ride-749654.netlify.com/', headers: :any, methods: [:get, :post, :patch, :delete, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
