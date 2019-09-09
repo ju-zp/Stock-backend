c799ec128e3cee8e6bec2845226d56cfc9661b73
ju-zp <jhuntington@tutanota.com>
2019-09-09 10:51:30 +0100

    updated code to only return information for available stock

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -4,21 +4,27 @@ class StockController < ApplicationController
   before_action :get_product, only: [:in_stock]
 
   def product_list
-    @products = Product.all
-    @newArray = []
-    @products.map{|p| @newArray.push(TransformerHelper::ProductList.transform_product_info(p))}
-    render json: @newArray.reject{|i| !i}
+    products = Product.all
+    newArray = []
+    products.map{|p| newArray.push(TransformerHelper::ProductList.transform_product_info p )}
+    render json: newArray.reject{|i| !i}
   end
 
   def in_stock
-    @orderProducts = @product.batches.order({ best_before: :asc }).select{|b| b.get_sold != b.quantity}
-    @newArr = []
-    @orderProducts.map{|b| @newArr.push(transform_batch(b, @product.name))} 
-    render json: @newArr
+    orderProducts = @product.batches.order({ best_before: :asc })
+    newArr = []
+    orderProducts.map{|b| newArr.push(transform_batch b )}
+    if newArr[0]
+      render json: newArr
+    else
+      render json: { message: 'There is no stock available for this product'}
+    end
   end
 
-  def transform_batch(batch, name)
-    { code: batch[:code], stock: batch[:quantity] - batch.get_sold, best_before: batch[:best_before] }
+  def transform_batch batch 
+    if batch[:quantity] - batch.get_sold > 0
+      { code: batch[:code], stock: batch[:quantity] - batch.get_sold, best_before: batch[:best_before] }
+    end
   end
 
   private 
