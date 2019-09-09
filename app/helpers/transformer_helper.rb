5ee237a68c971e63f4d972bfe22f9b18cdef0e09
ju-zp <jhuntington@tutanota.com>
2019-09-09 10:36:07 +0100

    cleaned up code and made use of ruby syntax more

--- app/helpers/transformer_helper.rb
+++ app/helpers/transformer_helper.rb
@@ -2,22 +2,32 @@ module TransformerHelper
 
   class ProductList
 
-    def self.transform_product_info(product)
-      @batches = product.batches
-      @newArr = []
-      @batches.map do |b|
-        if(0 < b.quantity - b.get_sold)
-          @newArr.push(self.transform_batch(b, product.name))
+    def self.transform_product_info product
+      batches = product.batches
+      newArr = []
+
+      batches.map do |b|
+
+        if 0 < b.quantity - b.get_sold 
+          newArr.push(self.transform_batch b )
         end
+
       end
-      if(!@newArr.empty?)
-        {product: product[:name], batches: @newArr}
+      
+      if !newArr.empty?
+        {product: product[:name], batches: newArr}
       end
     end
 
-    def self.transform_batch(batch, name)
+    def self.transform_batch batch 
       { code: batch[:code], quantity: batch[:quantity], product_id: batch[:product_id], sold: batch.get_sold}
     end
     
+  end
+
+  class Stock
+
+    def self.transform_batch()
+    end
   end
 end
