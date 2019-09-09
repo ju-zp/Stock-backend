64e274314a78f96bda34eb214bb97f7505d359cd
ju-zp <jhuntington@tutanota.com>
2019-09-09 10:21:12 +0100

    moved stocking endpoint into stock controller

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -8,4 +8,15 @@ class StockController < ApplicationController
     render json: @newArray.reject{|i| !i}
   end
 
+  def in_stock
+    @orderProducts = @product.batches.order({ best_before: :asc }).select{|b| b.get_sold != b.quantity}
+    @newArr = []
+    @orderProducts.map{|b| @newArr.push(transform_batch(b, @product.name))} 
+    render json: @newArr
+  end
+
+  def transform_batch(batch, name)
+    {id: batch[:id], code: batch[:code], quantity: batch[:quantity], product: name, product_id: batch[:product_id], best_before: batch[:best_before], sold: batch.get_sold}
+  end
+
 end
