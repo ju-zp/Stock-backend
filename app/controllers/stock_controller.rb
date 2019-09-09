10a1d47b7d4d0c54f2b5280d791c9b5ac54f912a
ju-zp <jhuntington@tutanota.com>
2019-09-09 09:31:01 +0100

    added functions for handling productlist endpoint

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -1,3 +1,23 @@
 class CustomController < ApplicationController
 
+  def product_list
+    @products = Product.all
+    @newArray = []
+    @products.map{|p| @newArray.push(transform_product(p))}
+    render json: @newArray
+  end
+
+
+  def transform_product(product)
+    @batches = product.batches
+    @newArr = []
+    @batches.map{|b| @newArr.push(transform_batch(b, product.name))}
+    {product: product, batches: @newArr}
+  end
+
+  def transform_batch(batch, name)
+    {id: batch[:id], code: batch[:code], quantity: batch[:quantity], product: name, product_id: batch[:product_id], best_before: batch[:best_before], sold: batch.get_sold}
+  end
+
+
 end
