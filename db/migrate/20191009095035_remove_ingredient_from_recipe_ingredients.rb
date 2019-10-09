7ea37eb99d5e1b0196cd6956a9394a0bde838175
ju-zp <jhuntington@tutanota.com>
2019-10-09 10:58:15 +0100

    ran migrations

--- db/migrate/20191009095035_remove_ingredient_from_recipe_ingredients.rb
+++ db/migrate/20191009095035_remove_ingredient_from_recipe_ingredients.rb
@@ -1,6 +1,6 @@
 class RemoveIngredientFromRecipeIngredients < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :recipe_ingredients, :ingredient_id, foreign_key: true
-    add_column :recipe_ingredients, :ingredient_stock_id, foreign_key: true
+    remove_reference :recipe_ingredients, :ingredient, foreign_key: true
+    add_reference :recipe_ingredients, :ingredient_stock, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_07_120735) do
+ActiveRecord::Schema.define(version: 2019_10_09_095035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,22 @@ ActiveRecord::Schema.define(version: 2019_07_07_120735) do
   create_table "batches", force: :cascade do |t|
     t.string "code"
     t.integer "quantity"
+    t.integer "sold"
     t.bigint "product_id"
     t.datetime "best_before"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_batches_on_product_id"
+  end
+
+  create_table "ingredient_stocks", force: :cascade do |t|
+    t.bigint "ingredient_id"
+    t.date "rec"
+    t.date "best_before"
+    t.date "shelf"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_ingredient_stocks_on_ingredient_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -58,25 +69,26 @@ ActiveRecord::Schema.define(version: 2019_07_07_120735) do
 
   create_table "recipe_ingredients", force: :cascade do |t|
     t.bigint "recipe_id"
-    t.bigint "ingredient_id"
     t.string "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.bigint "ingredient_stock_id"
+    t.index ["ingredient_stock_id"], name: "index_recipe_ingredients_on_ingredient_stock_id"
     t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.bigint "batch_id"
+    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["batch_id"], name: "index_recipes_on_batch_id"
+    t.index ["product_id"], name: "index_recipes_on_product_id"
   end
 
   add_foreign_key "batch_orders", "batches"
   add_foreign_key "batch_orders", "orders"
   add_foreign_key "batches", "products"
-  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "ingredient_stocks", "ingredients"
+  add_foreign_key "recipe_ingredients", "ingredient_stocks"
   add_foreign_key "recipe_ingredients", "recipes"
-  add_foreign_key "recipes", "batches"
+  add_foreign_key "recipes", "products"
 end
