fd4f538095a04863850e910637218e9bde366417
ju-zp <jhuntington@tutanota.com>
2019-10-11 16:39:50 +0100

    changes

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -1,10 +1,18 @@
 class IngredientController < ApplicationController 
 
+  before_action :get_product, only: [:create]
+
   def create
     params[:ingredients].each do |i|
       puts i
     end
     byebug
+  end
+
+  private
+
+  def get_product
+    @product = Product.find_by slug: params[:product]
   end
 
 end
