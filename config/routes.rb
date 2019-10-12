a1a1a53056949444b7bb59f19864804528f92d53
ju-zp <jhuntington@tutanota.com>
2019-10-12 16:52:08 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/productlist', to: 'stock#product_list'
   get '/product/:slug/stock', to: 'stock#in_stock'
   get '/product/:slug/orders', to: 'product#orders'
-  resources :batch, :order, :recipe
+  resources :batch, :order, :recipe, :ingredient
 end
